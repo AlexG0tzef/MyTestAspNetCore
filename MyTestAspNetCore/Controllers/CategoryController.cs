dcde2e4963e9370a952f10383e36f4c13330c686
Alex Götzef <g0tzef@yandex.ru>
2022-04-01 12:22:25 +0300

    test

---
+++ MyTestAspNetCore/Controllers/CategoryController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyTestAspNetCore.Data;
+using MyTestAspNetCore.Models;
+
+namespace MyTestAspNetCore.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext contextDb;
+        public CategoryController(ApplicationDbContext ContextDb)
+        {
+            contextDb = ContextDb;
+        }
+        public IActionResult Index()
+        {
+            List<CategoryM> objCategoryList = contextDb.Categories.ToList();
+            return View(objCategoryList);
+        }
+        //GET METHOD
+        public IActionResult Create()
+        {
+            return View();
+        }
+        //POST METHOD
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(CategoryM obj)
+        {
+            if (ModelState.IsValid)
+            {
+                contextDb.Categories.Add(obj);
+                contextDb.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+        }
+    }
+}
---
+++ MyTestAspNetCore/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MyTestAspNetCore.Models;
+
+namespace MyTestAspNetCore.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        { 
+        }
+
+        public DbSet<CategoryM> Categories { get; set; }
+    }
+}
