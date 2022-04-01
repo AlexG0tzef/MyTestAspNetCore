3728d11d3e7422b20dcbec98278a6301375b0d79
Alex Götzef <g0tzef@yandex.ru>
2022-04-01 13:39:36 +0300

    test

--- MyTestAspNetCore/Controllers/CategoryController.cs
+++ MyTestAspNetCore/Controllers/CategoryController.cs
@@ -16,6 +16,8 @@ namespace MyTestAspNetCore.Controllers
             List<CategoryM> objCategoryList = contextDb.Categories.ToList();
             return View(objCategoryList);
         }
+
+        #region Create
         //GET METHOD
         public IActionResult Create()
         {
@@ -26,13 +28,86 @@ namespace MyTestAspNetCore.Controllers
         [ValidateAntiForgeryToken]
         public IActionResult Create(CategoryM obj)
         {
+            if (obj.Name.Contains("!")) 
+            {
+                ModelState.AddModelError("Name", "Test Error");
+            }
             if (ModelState.IsValid)
             {
                 contextDb.Categories.Add(obj);
                 contextDb.SaveChanges();
+                TempData["success"] = "Category created successfully";
                 return RedirectToAction("Index");
             }
             return View(obj);
         }
+        #endregion
+
+        #region Edit
+        //GET METHOD
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            else
+            {
+                var categoryFromDb = contextDb.Categories.Find(id);
+                if (categoryFromDb == null) NotFound();
+                return View(categoryFromDb);
+            }
+        }
+        //POST METHOD
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(CategoryM obj)
+        {
+            if (obj.Name.Contains("!"))
+            {
+                ModelState.AddModelError("Name", "Test Error");
+            }
+            if (ModelState.IsValid)
+            {
+                contextDb.Categories.Update(obj);
+                contextDb.SaveChanges();
+                TempData["success"] = "Category updated successfully";
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+        }
+        #endregion
+
+        #region Delete
+        //GET METHOD
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            else
+            {
+                var categoryFromDb = contextDb.Categories.Find(id);
+                if (categoryFromDb == null) NotFound();
+                return View(categoryFromDb);
+            }
+        }
+        //POST METHOD
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeletePost(int? id)
+        {
+            var obj = contextDb.Categories.Find(id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            contextDb.Categories.Remove(obj);
+            contextDb.SaveChanges();
+            TempData["success"] = "Category deleted successfully";
+            return RedirectToAction("Index");
+        }
+        #endregion
     }
 }
